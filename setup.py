d3af64e7752de08f94b7fad60add9fb52c1a7d21
Brian Luft <brian@unbracketed.com>
2011-02-04 06:56:35 -0800

    changes version to 0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "python-flot-utils",
-    version = "0.0.1",
+    version = "0.1",
     author = "Brian Luft",
     author_email = "brian@unbracketed.com",
     description = ("Makes it easy to convert Python data structures" 
